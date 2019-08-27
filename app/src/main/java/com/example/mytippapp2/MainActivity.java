85afc6e1bf8a6b1fcd1ec4e6527191f8aecf04c8
cameron_chiu <cameronchiu31@icloud.com>
2019-08-26 23:30:32 -0500

    All functional w/ display quirks

--- app/src/main/java/com/example/mytippapp2/MainActivity.java
+++ app/src/main/java/com/example/mytippapp2/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import java.text.DecimalFormat;
@@ -32,5 +33,19 @@ public class MainActivity extends AppCompatActivity {
         tip = Percentage*.01*Bill;
         textViewTipOutput.setText(""+df.format(tip));
 
+    }
+
+    public void calculateQualityTip(View v){
+        TextView textViewTipOutput2 = (TextView)findViewById(R.id.textViewTipOutput2);
+        EditText bill2 = (EditText) findViewById(R.id.editText2);
+        EditText percentage2 = (EditText) findViewById(R.id.editTextPercentage2);
+        RatingBar ratingBar = (RatingBar) findViewById(R.id.ratingBar);
+
+        double Bill2 = Double.parseDouble(bill2.getText().toString());
+        double Percentage2 = Double.parseDouble(percentage2.getText().toString());
+        double tip = 0;
+        double extra = ratingBar.getRating();
+        tip = Percentage2*.01*Bill2+extra;
+        textViewTipOutput2.setText(""+df.format(tip));
     }
 }
